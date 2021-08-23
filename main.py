331e58aef4da534dd63a23fa81bf2a75d1a7d704
Ashish Papanai <ashishpapanai00@gmail.com>
2021-08-23 08:09:50 +0530

    Added eyes, nose and mouth detection

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import cv2
 from mtcnn import MTCNN
 import display
 img = (cv2.imread("images/test3.jpg"))
-img = cv2.resize(img, (1280, 720), interpolation = cv2.INTER_NEAREST)
-
-
+img = cv2.resize(img, (600, 600), interpolation = cv2.INTER_NEAREST)
 
 detector  = MTCNN()
 faces = detector.detect_faces(img)
 
 for i in range(0, len(faces)):
-    x, y, width, height = faces[i]['box']
+    result = faces[i]
+    x, y, width, height = result['box']
     x2 = x + width
     y2 = y + height
-    # create the shape
     img = cv2.rectangle(img, (x, y), (x2, y2), (0,0,255), 1)
-
+    # create the shape
+    for key, value in result['keypoints'].items():
+			# create and draw dot
+            img = cv2.circle(img, value, radius=2, color=(0,0,255))
+            
 display.Display(img)
