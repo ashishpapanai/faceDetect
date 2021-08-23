765901f930c7b7da7a2e762cf5b230e704a5d00d
Ashish Papanai <ashishpapanai00@gmail.com>
2021-08-23 08:00:15 +0530

    Face Detection: Added

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import cv2
 from mtcnn import MTCNN
 import display
-img = (cv2.imread("images/test2.jpg"))
-#img = cv2.resize(img, (780, 540), interpolation = cv2.INTER_NEAREST)
+img = (cv2.imread("images/test3.jpg"))
+img = cv2.resize(img, (1280, 720), interpolation = cv2.INTER_NEAREST)
 
 
 
-'''detector  = MTCNN()
+detector  = MTCNN()
 faces = detector.detect_faces(img)
-box = result[0]['box']
-x, y, width, height = box
-x2, y2 = x + width, y + height
-print(box)'''
+
+for i in range(0, len(faces)):
+    x, y, width, height = faces[i]['box']
+    x2 = x + width
+    y2 = y + height
+    # create the shape
+    img = cv2.rectangle(img, (x, y), (x2, y2), (0,0,255), 1)
+
+display.Display(img)
